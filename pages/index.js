04f37ca926f2d1f1eba2525d044f4229cbae4be6
BlinkyDEFI <blinkyonsol@gmail.com>
2025-05-22 01:30:36 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ import { setComputeUnitLimit } from '@metaplex-foundation/mpl-toolbox';
 /*
    CONFIG:
    - Candy Machine & Guard
-   - BLINKY tokenPayment => 1060000 tokens
+   - BLINKY tokenPayment => 400 tokens
    - mintLimit => id=1
 */
 
